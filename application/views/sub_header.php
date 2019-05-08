f08f2ec900efbc425fca1e7c59ba11203d372672
dawinksd78 <buda00@naver.com>
2019-05-08 15:16:04 +0900

    페이스북 pixel code 적용

--- application/views/sub_header.php
+++ application/views/sub_header.php
@@ -47,6 +47,19 @@ var openState = "<?php echo $this->config->item('openAlert'); ?>";
 <!-- private setup -->
 <script src="/js/mobile_menu.js?ver=<?php echo $this->config->item('js_css_ver')?>"></script>
 <script src="/js/underscore.js"></script>
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2410886338974162'); 
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" src="https://www.facebook.com/tr?id=2410886338974162&ev=PageView&noscript=1"/></noscript>
+<!-- End Facebook Pixel Code -->
 </head>
 
 <body>
